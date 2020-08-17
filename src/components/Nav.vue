3615fc4c83b54dc6a920f693d039205dcb386082
Renato-Z <906155099@qq.com>
2020-08-17 16:11:26 +0800

    回dev建立登录新分支

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="nav_bar">
     <el-menu
       style="text-align: center;width: 100%"
       :default-active="activeIndex"
@@ -22,6 +22,45 @@
         <router-link to="/rankings">排行榜</router-link>
       </el-menu-item>
     </el-menu>
+    <div class="login_btn">
+      <el-button type="text" @click="loginPanelShow = true">登录</el-button>
+    </div>
+    <div class="login_panel" v-show="loginPanelShow">
+      <el-card class="login_card">
+        <i class="close el-icon-close" @click="loginPanelShow = false"></i>
+        <h4 class="title">
+          <a :class="{ active: active == 'login' }" @click="active = 'login'"
+            >登录</a
+          >
+          <b>·</b>
+          <a :class="{ active: active == 'signUp' }" @click="active = 'signUp'"
+            >注册</a
+          >
+        </h4>
+        <el-form :rules="rules" ref="ruleForm" class="login_form">
+          <el-form-item>
+            <el-input
+              class="user_name_input"
+              v-model="loginForm.userName"
+              prefix-icon="el-icon-user"
+              placeholder="用户名"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-input
+              class="password_input"
+              type="password"
+              v-model="loginForm.password"
+              prefix-icon="el-icon-lock"
+              placeholder="密码"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <button class="login_button" @click="login()">登录</button>
+          </el-form-item>
+        </el-form>
+      </el-card>
+    </div>
   </div>
 </template>
 
@@ -30,15 +69,152 @@ export default {
   name: "nav",
   data() {
     return {
-      activeIndex: "1"
+      activeIndex: "1",
+      loginPanelShow: false, // 是否显示登录面板
+      active: "login",
+      loginForm: {
+        userName: "",
+        password: ""
+      },
+      rules: {
+        userName: [
+          { required: true, message: "请输入用户名", trigger: "blur" }
+        ],
+        password: [{ required: true, message: "请输入密码", trigger: "blur" }]
+      }
     };
   },
   methods: {
     handleSelect(key, keyPath) {
       console.log(key, keyPath);
-    }
+    },
+    login() {}
   }
 };
 </script>
 
-<style scoped></style>
+<style scoped lang="less">
+.nav_bar {
+  position: relative;
+  /deep/ .login_btn {
+    position: absolute;
+    right: 20px;
+    top: 50%;
+    transform: translateY(-50%);
+    .el-button {
+      color: #fff;
+      height: 30px;
+      line-height: 30px;
+      padding: 0 10px;
+      border: 1px solid #ffd04b;
+      &:hover {
+        color: #ffd04b;
+      }
+    }
+  }
+}
+.login_panel {
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.6);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top: 0;
+  left: 0;
+  z-index: 1000;
+  /deep/ .login_card {
+    position: relative;
+    width: 400px;
+    margin: 60px auto 0;
+    padding-top: 46px;
+    padding-bottom: 30px;
+    background-color: #fff;
+    border-radius: 4px;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.1);
+    vertical-align: middle;
+    display: inline-block;
+    transform: translateY(-30%);
+    .el-card__body {
+      padding-top: 0;
+    }
+    .login_form {
+      padding: 0 40px;
+      .login_button {
+        background: #3194d0;
+        width: 100%;
+        padding: 9px 18px;
+        font-size: 18px;
+        border: none;
+        border-radius: 25px;
+        color: #fff;
+        cursor: pointer;
+        outline: none;
+        display: block;
+        margin-top: 30px;
+        &:hover {
+          background: #187cb7;
+        }
+      }
+      .el-form-item {
+        margin-bottom: 0;
+      }
+      .el-form-item__content {
+        line-height: 37px;
+      }
+      .el-input__inner:focus {
+        border-color: #c0c4cc;
+      }
+      .el-input__inner {
+        border-color: #c0c4cc;
+      }
+      input {
+        background-color: hsla(0, 0%, 71%, 0.1);
+        outline: none;
+      }
+      .user_name_input > input {
+        border-radius: 4px 4px 0 0;
+        border-bottom: 0;
+      }
+      .password_input > input {
+        border-radius: 0 0 4px 4px;
+      }
+    }
+
+    .close {
+      position: absolute;
+      font-size: 36px;
+      right: 10px;
+      top: 10px;
+      &:hover {
+        color: #ffd04b;
+      }
+    }
+    .title {
+      margin: 0 auto 40px;
+      padding: 10px;
+      font-weight: 400;
+      color: #969696;
+      a {
+        // cursor: pointer;
+        font-size: 18px;
+        font-weight: 700;
+        padding: 10px;
+        color: #969696;
+        text-decoration: none;
+        &:hover {
+          border-bottom: 2px solid #ffd04b;
+        }
+        &.active {
+          color: #ffd04b;
+          border-bottom: 2px solid #ffd04b;
+        }
+        b {
+          padding: 10px;
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/Rankings/Rankings.vue
@@ -0,0 +1,109 @@
+<template>
+  <div id="Rankings_container">
+    <!-- 昵称 -->
+    <!-- 当月打卡次数 -->
+    <!-- 被精选次数 -->
+    <el-table :data="rankingsData" style="width: 100%">
+      <!-- <el-table-column prop="date" label="排名">
+        <template slot-scope="scope">
+          <span class="top" :class="topStyle(scope.row)">
+            {{ scope.row.rank }}
+          </span>
+        </template>
+      </el-table-column> -->
+      <el-table-column type="index" label="排名"> </el-table-column>
+      <el-table-column prop="name" label="昵称"> </el-table-column>
+      <el-table-column prop="selects" label="当月打卡次数"> </el-table-column>
+      <el-table-column prop="checks" label="被精选次数"> </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Rankings",
+  props: {},
+  components: {},
+  data() {
+    return {
+      rankingsData: [
+        {
+          name: "lucifer",
+          selects: 0,
+          checks: 0
+        },
+        {
+          name: "lucifer",
+          selects: 0,
+          checks: 0
+        },
+        {
+          name: "lucifer",
+          selects: 0,
+          checks: 0
+        },
+        {
+          name: "lucifer",
+          selects: 0,
+          checks: 0
+        },
+        {
+          name: "lucifer",
+          selects: 0,
+          checks: 0
+        }
+      ]
+    };
+  },
+  created() {},
+  mounted() {},
+  methods: {
+    topStyle(top) {
+      console.log(top);
+      switch (top) {
+        case 1:
+          return "top1";
+        case 2:
+          return "top2";
+        case 3:
+          return "top3";
+        default:
+          return "";
+      }
+    }
+  }
+};
+</script>
+
+<style scoped lang="less">
+#Rankings_container {
+  /deep/ .el-table {
+    // 排名数字
+
+    th,
+    td {
+      text-align: center;
+    }
+    .top {
+      display: inline-block;
+      text-align: center;
+      width: 10px;
+      font-size: 16px;
+      font-family: Roboto-Medium, Roboto;
+      font-weight: 500;
+      color: rgba(0, 0, 0, 0.45);
+      line-height: 22px;
+      margin-right: 16px;
+    }
+    .top1 {
+      color: rgba(234, 46, 46, 1);
+    }
+    .top2 {
+      color: rgba(155, 159, 195, 1);
+    }
+    .top3 {
+      color: rgba(246, 159, 18, 1);
+    }
+  }
+}
+</style>
