47d538cdf4fce3fc2efe24ae875540ec9a56c984
mengchen007 <mengchen0007@163.com>
2020-08-23 21:51:06 +0800

    去掉不使用的依赖

--- src/api/exercises.js
+++ src/api/exercises.js
@@ -3,7 +3,7 @@ import request from '@/utils/request'
 // 获取所有题解
 export function allExercises() {
   return request({
-    url: '/15/api/allExercises/period',
+    url: '/api/allExercises/period',
     method: 'get'
   })
 }
