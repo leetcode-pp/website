d28d6e548bfc9fd1a86da740514781517ef73dd8
Renato-Z <906155099@qq.com>
2020-08-24 17:45:29 +0800

    登录和排行榜使用mock数据

--- src/api/login.js
+++ src/api/login.js
@@ -3,7 +3,15 @@ import request from '@/utils/request'
 // 登录方法
 export function login (data) {
   return request({
-    url: '/api/userSignin',
+    url: '/api/v1/userSignin',
+    method: 'post',
+    data: data
+  })
+}
+// 注册
+export function signUp (data) {
+  return request({
+    url: '/api/v1/signup',
     method: 'post',
     data: data
   })
